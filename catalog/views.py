342668b27059cb1450233e375c65f47d11915f43
YaroslavVolvach <60551329+YaroslavVolvach@users.noreply.github.com>
2020-11-12 15:40:09 +0200

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,6 @@ from .models import *
 
 
 def product_list (request):
-    s = 'catalog_list'
     return render(request, 'catalog/product_list.html', context={'categories': Category.objects.all()})
 
 
